6c74fe11071df85192d74581a021ecf67c13edb8
Andre M. Bertachini <andre@bertachini.org>
2025-04-20 12:09:22 -0300

    Options and keymaps

--- init.lua
+++ init.lua
@@ -1,4 +1,8 @@
-require("plugins.lazy")
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require 'config.options'
+require 'plugins.lazy'
 
 require("lazy").setup({
 	require 'plugins/telescope',
