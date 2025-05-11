7eb76a2632cd7045ebe39de1b00c2ffc2a8a11c5
Andre M. Bertachini <andre@bertachini.org>
2025-05-11 12:21:19 -0300

    Treesitter

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ require('lazy').setup({
     require('plugins/mason'),
     require('plugins/lspconfig'),
     require('plugins/conform'),
+    require('plugins/treesitter'),
 })
 
 require('config.options')
